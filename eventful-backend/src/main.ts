e14c27488f28364364790a8a6617798f7ed99223
Mahmud Ghali Abdulhamid <mahmudghali01@gmail.com>
2026-05-29 00:34:51 +0100

    add global validation pipe

--- eventful-backend/src/main.ts
+++ eventful-backend/src/main.ts
@@ -1,12 +1,13 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Eventful API Engine')
@@ -23,7 +24,7 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
   console.log(` Eventful server is running on: http://localhost:3000`);
   console.log(
-    `Swagger Documentation available at: http://localhost:3000/api/docs`
+    `Swagger Documentation available at: http://localhost:3000/api/docs`,
   );
 }
 bootstrap();

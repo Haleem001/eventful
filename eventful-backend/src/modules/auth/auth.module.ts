0aecac771430aaea0829b1ee0bff493e38507ed3
Mahmud Ghali Abdulhamid <mahmudghali01@gmail.com>
2026-05-29 00:33:44 +0100

    add auth controller and auth service to auth module

--- eventful-backend/src/modules/auth/auth.module.ts
+++ eventful-backend/src/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}

5f2806a4328e59022a4ee2163d2b84c152ff699a
Mahmud Ghali Abdulhamid <mahmudghali01@gmail.com>
2026-05-29 02:38:07 +0100

    initialise typeorm and ticket service in ticket module

--- eventful-backend/src/modules/tickets/tickets.module.ts
+++ eventful-backend/src/modules/tickets/tickets.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ticket } from './entities/ticket.entity';
+import { TicketsService } from './tickets.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Ticket])],
+  providers: [TicketsService],
+  exports: [TypeOrmModule, TicketsService],
+})
 export class TicketsModule {}

33d47dac05cea79c4d73f52719fd23a3352e97c1
Mahmud Ghali Abdulhamid <mahmudghali01@gmail.com>
2026-05-29 03:00:13 +0100

    refactor JwtAuthGuard to include ExecutionContext in handleRequest method

--- eventful-backend/src/common/guards/jwt-auth.guard.ts
+++ eventful-backend/src/common/guards/jwt-auth.guard.ts
@@ -1,9 +1,9 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(err: Error | null, user: unknown): unknown {
+  handleRequest<TUser = any>(err: any, user: any, _info: any, _context: ExecutionContext, _status?: any): TUser {
     if (err || !user) {
       throw err || new UnauthorizedException();
     }
